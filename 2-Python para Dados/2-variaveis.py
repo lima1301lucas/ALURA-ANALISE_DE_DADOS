ec5c6ac021da48efe01d7cc2acaad4de73747aa3
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2026-01-16 21:03:41 -0300

    Aulas 2.2.1-2.2.4

---
+++ 2-Python para Dados/2-variaveis.py
@@ -0,0 +1,11 @@
+idade = 5
+print(idade)
+
+idade = 10
+print(idade)
+
+idade = 15
+print(idade)
+
+nome = 'Lucas'
+print(nome)
---
+++ 2-Python para Dados/4-variaveisnumericas.py
@@ -0,0 +1,38 @@
+#q_seguranca = 5
+#s_seguranca = 3000
+#
+#q_docente = 16
+#s_docente = 6000
+#
+#q_diretoria = 1
+#s_diretoria = 12500
+#
+#total_empregados = q_diretoria + q_docente + q_seguranca
+#print(total_empregados)
+#
+#diferenca_salario = s_diretoria - s_seguranca
+#print(diferenca_salario)
+#
+#media = (q_seguranca * s_seguranca + q_docente * s_docente + q_diretoria * s_diretoria) / (total_empregados)
+#print(media)
+
+potenciacao = 2 ** 3
+print(potenciacao)
+
+operador = 2
+potencia = 3
+print(operador ** potencia)
+
+resto_divisao = 7 % 3
+print(resto_divisao)
+
+dividendo = 7
+divisor = 3
+print(dividendo % divisor)
+
+divisao_inteira = 7 // 3
+print(divisao_inteira)
+
+numerador = 7
+denominador = 3
+print(numerador // denominador)
